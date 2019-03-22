f75a405a5e8bb466132e6e906c396f490c23e256
lkito <lkiti17@freeuni.edu.ge>
2019-03-22 23:54:25 +0400

    Finished writing Tetris grid class

--- TetrisGrid.java
+++ TetrisGrid.java
@@ -4,13 +4,21 @@
 package assign1;
 
 public class TetrisGrid {
-	
+
+	private boolean[][] grid;
+
 	/**
 	 * Constructs a new instance with the given grid.
 	 * Does not make a copy.
 	 * @param grid
 	 */
 	public TetrisGrid(boolean[][] grid) {
+		this.grid = new boolean[grid.length][grid[0].length];
+		for(int i = 0; i < grid.length; i++){
+			for(int k = 0; k < grid[i].length; k++){
+				this.grid[i][k] = grid[i][k];
+			}
+		}
 	}
 	
 	
@@ -18,6 +26,30 @@ public class TetrisGrid {
 	 * Does row-clearing on the grid (see handout).
 	 */
 	public void clearRows() {
+		int cleared = 0;
+		boolean isFull;
+		for(int j = 0; j < grid[0].length; j++){
+			isFull = true;
+			for(int i = 0; i < grid.length; i++){
+				if(!grid[i][j]){
+					isFull = false;
+					break;
+				}
+			}
+			if(isFull){
+				cleared++;
+			} else {
+				if (cleared == 0) continue;
+				for (int i = 0; i < grid.length; i++) {
+					grid[i][j - cleared] = grid[i][j];
+				}
+			}
+		}
+		for(int i = grid[0].length - cleared; i < grid[0].length; i++) {
+			for (int j = 0; j < grid.length; j++) {
+				grid[j][i] = false;
+			}
+		}
 	}
 	
 	/**
@@ -25,6 +57,6 @@ public class TetrisGrid {
 	 * @return 2d grid array
 	 */
 	boolean[][] getGrid() {
-		return null; // TODO YOUR CODE HERE
+		return this.grid;
 	}
 }
