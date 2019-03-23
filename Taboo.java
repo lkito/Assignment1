7252092ada6cf2a3ba580d6e3ef55c88f88d86aa
lkito <lkiti17@freeuni.edu.ge>
2019-03-23 23:54:04 +0400

    fixed some of Taboo tests, finished writing Taboo class. Assignment most likely done, phew!

--- Taboo.java
+++ Taboo.java
@@ -9,12 +9,31 @@ package assign1;
 import java.util.*;
 
 public class Taboo<T> {
+
+	private Map<T, HashSet<T> > ruleMap;
 	
 	/**
 	 * Constructs a new Taboo using the given rules (see handout.)
 	 * @param rules rules for new Taboo
 	 */
 	public Taboo(List<T> rules) {
+		this.ruleMap = new HashMap<>();
+		Iterator<T> it = rules.iterator();
+		T cur;
+		T last = null;
+		if(it.hasNext()) last = it.next();
+		while(it.hasNext()){
+			cur = it.next();
+			if(cur == null){
+				last = null;
+				continue;
+			}
+			if(last != null){
+				if(!ruleMap.containsKey(last)) ruleMap.put(last, new HashSet<>());
+				ruleMap.get(last).add(cur);
+			}
+			last = cur;
+		}
 	}
 	
 	/**
@@ -24,7 +43,8 @@ public class Taboo<T> {
 	 * @return elements which should not follow the given element
 	 */
 	public Set<T> noFollow(T elem) {
-		 return null; // TODO YOUR CODE HERE
+		if(!ruleMap.containsKey(elem)) return Collections.emptySet();
+		 return ruleMap.get(elem);
 	}
 	
 	/**
@@ -33,5 +53,21 @@ public class Taboo<T> {
 	 * @param list collection to reduce
 	 */
 	public void reduce(List<T> list) {
+		T last = null;
+		if(!list.isEmpty()){
+			last = list.get(0);
+		}
+		int size = list.size();
+		for(int i = 1; i < size; i++){
+			if(ruleMap.containsKey(last) && ruleMap.get(last).contains(list.get(i))){
+				list.remove(i);
+				size--;
+				i--;
+			} else last = list.get(i);
+		}
 	}
 }
+
+
+
+
--- TabooTest.java
+++ TabooTest.java
@@ -28,21 +28,19 @@ public class TabooTest {
         Taboo<Integer> tab = new Taboo<>(rule);
         assertEquals(Collections.emptySet(), tab.noFollow(100));
 
-        Set<Integer> set = new HashSet<>(Arrays.asList(2));
+        Set<Integer> set = new HashSet<>(Arrays.asList(2, 100));
         assertEquals(set, tab.noFollow(2));
     }
 
     @Test
     public void tabooTest3(){
-        List<Integer> rule = Arrays.asList(2, 2, 2, 2, null, 2, 2, 2);
+        List<Integer> rule = new ArrayList<>(Arrays.asList(2, 2, 2, 2, null, 2, 2, 2));
         Taboo<Integer> tab = new Taboo<>(rule);
-        tab.reduce(rule);
-        assertEquals(Arrays.asList(2), rule);
 
-        List<Integer> test = Arrays.asList(1, 2, 5, null, 10, 5);
+        List<Integer> test = new ArrayList<>(Arrays.asList(1, 2, 5, 10, 5)); //Arrays.asList(1, 2, 5, 10, 5);
         tab.reduce(test);
-        assertEquals(Arrays.asList(1, 2, 5, null, 10, 5), test);
-        test = Arrays.asList();
+        assertEquals(Arrays.asList(1, 2, 5, 10, 5), test);
+        test = new ArrayList<>(Arrays.asList());
         tab.reduce(test);
         assertEquals(Arrays.asList(), test);
     }
@@ -52,7 +50,7 @@ public class TabooTest {
         List<Integer> rule = Arrays.asList(10, 534, null, 534, 1000, 534, 22, 22, 534, 2000);
         Taboo<Integer> tab = new Taboo<>(rule);
 
-        List<Integer> test = Arrays.asList(534, 1000, 1000, 1000, 22, 534, 2000, 10, 10, 2000);
+        List<Integer> test = new ArrayList<>(Arrays.asList(534, 1000, 1000, 1000, 22, 534, 2000, 10, 10, 2000));
         tab.reduce(test);
         assertEquals(Arrays.asList(534, 534, 10, 10, 2000), test);
     }
@@ -62,7 +60,7 @@ public class TabooTest {
         List<Integer> rule = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
         Taboo<Integer> tab = new Taboo<>(rule);
 
-        List<Integer> test = Arrays.asList(1, 2, 4, 5, 6, 2, 9, 8, 7, 8, 3, 3, 3, 2);
+        List<Integer> test = new ArrayList<>(Arrays.asList(1, 2, 4, 5, 6, 2, 9, 8, 7, 8, 3, 3, 3, 2));
         tab.reduce(test);
         assertEquals(Arrays.asList(1, 4, 6, 2, 9, 7, 3, 3, 3), test);
     }
@@ -72,8 +70,8 @@ public class TabooTest {
         List<String> rule = Arrays.asList("bla", "blu", "bli", "Bla", "blo", "blo");
         Taboo<String> tab = new Taboo<>(rule);
 
-        List<String> test = Arrays.asList("bla", "bla", "blu", "bli", "bla", "blo",
-                "blo", "Bla", "blo", "blo", "blue");
+        List<String> test = new ArrayList<>(Arrays.asList("bla", "bla", "blu", "bli", "bla", "blo",
+                "blo", "Bla", "blo", "blo", "blue"));
         tab.reduce(test);
         assertEquals(Arrays.asList("bla", "bla", "bli", "bla", "blo", "Bla", "blue"), test);
     }
@@ -83,8 +81,8 @@ public class TabooTest {
         List<Character> rule = Arrays.asList('A', 'a', null, 'B', 'b', null, 'C', 'c', null, 'D', 'd', null);
         Taboo<Character> tab = new Taboo<>(rule);
 
-        List<Character> test = Arrays.asList('A', 'a', 'a', 'a', 'a', 'B', 'b', 'b', 'b',
-                'A', 'a', 'a', 'R', 'A', 'a', '!');
+        List<Character> test = new ArrayList<>(Arrays.asList('A', 'a', 'a', 'a', 'a', 'B', 'b', 'b', 'b',
+                'A', 'a', 'a', 'R', 'A', 'a', '!'));
         tab.reduce(test);
         assertEquals(Arrays.asList('A', 'B', 'A', 'R', 'A', '!'), test);
     }
